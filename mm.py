f60f49c9855c61515a679db9d50befdb7db010f6
lungfachang-hash <lungfa.chang@gmail.com>
2026-05-26 20:22:45 +0800

    Update mm.py

--- mm.py
+++ mm.py
@@ -9,8 +9,8 @@ st.caption("授權標註：edit by 闕河正 | 專屬資淺初學者講義")
 
 st.write("---")
 
-# 2. 呼叫 st.columns(3)，在網頁橫向切出三個一模一樣寬度的大直欄變數
-col1, col2, col3 = st.columns(3)
+# 2. 呼叫 st.columns(4)，在網頁橫向切出三個一模一樣寬度的大直欄變數
+col1, col2, col3, col4 = st.columns(3)
 
 # 3. 運用 with 語法，像填空一樣把文字塞進對應的直欄空間裡
 with col1:
@@ -22,5 +22,9 @@ with col2:
     st.write("這裡未來要放『執行中』的卡片")
 
 with col3:
+    st.markdown("### Done (已完成)")
+    st.write("這裡未來要放『已完成』的卡片")
+
+with col4:
     st.markdown("### Done (已完成)")
     st.write("這裡未來要放『已完成』的卡片")
